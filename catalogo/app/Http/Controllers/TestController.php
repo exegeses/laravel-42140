12c09fce36b1fbe574a768b0c74b5b924ad3bf05
marcos pinardi <exegeses@gmail.com>
2020-11-17 22:02:39 -0300

    peticion y vista adminmarcas

---
+++ catalogo/app/Http/Controllers/TestController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+    //index
+    public function listarMarcas()
+    {
+
+    }
+
+    //show
+    public function verMarcaPorID()
+    {
+
+    }
+
+    // create & store
+    public function agregarMarca()
+    {
+
+    }
+
+    // edit & update
+    public function modificarMarca()
+    {
+
+    }
+}
--- catalogo/routes/web.php
+++ catalogo/routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+##########################
+#### CRUD de marcas
+use App\Http\Controllers\MarcaController;
+
+Route::get('/adminMarcas', [ MarcaController::class, 'index' ] );
+
