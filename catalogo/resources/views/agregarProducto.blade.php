2f8150aea752f8bd40efe104200e66e0299dd555
marcos pinardi <exegeses@gmail.com>
2020-11-26 22:06:20 -0300

    old value en los input del form

--- catalogo/resources/views/agregarProducto.blade.php
+++ catalogo/resources/views/agregarProducto.blade.php
@@ -11,21 +11,25 @@
             @csrf
 
                 Nombre: <br>
-                <input type="text" name="prdNombre" class="form-control">
+                <input type="text" name="prdNombre"
+                       value="{{ old('prdNombre') }}"
+                       class="form-control">
                 <br>
                 Precio: <br>
                 <div class="input-group mb-2">
                     <div class="input-group-prepend">
                         <div class="input-group-text">$</div>
                     </div>
-                    <input type="number" name="prdPrecio" class="form-control" step="0.01">
+                    <input type="number" name="prdPrecio"
+                           value="{{ old('prdPrecio') }}"
+                           class="form-control" step="0.01">
                 </div>
                 <br>
                 Marca: <br>
                 <select name="idMarca" class="form-control" required>
                     <option value="">Seleccione una marca</option>
                 @foreach( $marcas as $marca )
-                    <option value="{{ $marca->idMarca }}">{{ $marca->mkNombre }}</option>
+                    <option {{ ( old('idMarca')==$marca->idMarca )?'selected':'' }} value="{{ $marca->idMarca }}">{{ $marca->mkNombre }}</option>
                 @endforeach
                 </select>
                 <br>
@@ -33,15 +37,18 @@
                 <select name="idCategoria" class="form-control" required>
                     <option value="">Seleccione una Categoría</option>
                 @foreach ( $categorias as $categoria )
-                    <option value="{{ $categoria->idCategoria }}">{{ $categoria->catNombre }}</option>
+                    <option {{ ( old('idCategoria')==$categoria->idCategoria )?'selected':'' }} value="{{ $categoria->idCategoria }}">{{ $categoria->catNombre }}</option>
                 @endforeach
                 </select>
                 <br>
                 Presentacion: <br>
-                <textarea name="prdPresentacion" class="form-control"></textarea>
+                <textarea name="prdPresentacion"
+                          class="form-control">"{{ old('prdPresentacion') }}</textarea>
                 <br>
                 Stock: <br>
-                <input type="number" name="prdStock" class="form-control" min="0">
+                <input type="number" name="prdStock"
+                       value="{{ old('prdStock') }}"
+                       class="form-control" min="0">
                 <br>
                 Imagen: <br>
 
